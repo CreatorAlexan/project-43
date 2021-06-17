1e97b9117569cfdc47b8c10be4e6eddef5f89949
Sancterr <81334236+Sancterr@users.noreply.github.com>
2021-06-16 22:36:09 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    text("A T E D A S B A",700,160)
+    fill("lightblue")
+    text("Hint: Stores information in one location",700,180)
+    fill("white")
+    text("C U T N I F O N",300,380)
+    fill("lightblue")
+    text("Hint: Can be used multiple times",300,400)
+    
+
+}
